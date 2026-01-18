0bb0bcc60bd0f37d1e863055cd79fe55fb5bb36f
Askyturi <hdsoaw123@outlook.com>
2026-01-18 17:06:37 -0500

    Template website

--- src/components/ui/src/components/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-function HelloWorld() {
-    const name = "Developer";
-    const today = new Date().toLocaleDateString();
-
-    return (
-        <div className="p-8 text-center">
-            <h1 className="text-4xl font-bold text-blue-600">Hello, {name}!</h1>
-        )
-    
